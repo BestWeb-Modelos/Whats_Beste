63c1b8af1a49a0eb5bcc456fc409466a7eec2a7a
Cals <88802566+victorcals@users.noreply.github.com>
2024-04-14 14:29:18 -0300

    Create Page login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./all-css/login-page.css";
+import besteweb from "./assets/besteweb.png";
 
 function App() {
   return (
- <h1>hello word </h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-lite">Bem vindo</span>
+
+            <span className="login-form-lite">
+              <img src={besteweb} alt="logo beste web" />
+            </span>
+
+            <div className="wpar-input">
+              <input className="input-login" type="email" />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wpar-input">
+              <input className="input-login" type="password" />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">login</button>
+            </div>
+
+            <div className="criar-login">
+              <span className="criar-login-pt1"></span>
+
+              <a className="criar-login-pt2" href="#">
+                criar conta
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
